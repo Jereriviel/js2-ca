a7e7a60a0ea79957e1c3e9cfb79319deb6cc7824
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2025-11-06 12:54:16 +0100

    Created loading skeleton for comments.

--- src/js/components/loadingSkeletons.ts
+++ src/js/components/loadingSkeletons.ts
@@ -45,15 +45,35 @@ export function cardSkeleton(): string {
 
 export function commentSkeleton(): string {
   return `
-<div role="status" class="max-w-sm animate-pulse">
-    <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
-    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
-    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
-    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
-    <span class="sr-only">Loading...</span>
-</div>
+ <div role="status" class="flex animate-pulse pt-4">
+            <div>
+              <div
+                class="mr-4 h-12 w-12 rounded-full bg-gray-200 dark:bg-gray-700"
+              ></div>
+            </div>
+            <div class="flex grow flex-col">
+              <div>
+                <div
+                  class="mb-2 h-2.5 max-w-[100px] rounded-full bg-gray-200 dark:bg-gray-700"
+                ></div>
+              </div>
+              <div class="flex flex-col gap-1 py-2">
+                <div>
+                  <div
+                    class="mb-2.5 h-2 max-w-[460px] rounded-full bg-gray-200 dark:bg-gray-700"
+                  ></div>
+                  <div
+                    class="mb-2.5 h-2 max-w-[460px] rounded-full bg-gray-200 dark:bg-gray-700"
+                  ></div>
+                </div>
+              </div>
+              <div>
+                <div
+                  class="mb-2.5 h-2 max-w-[80px] rounded-full bg-gray-200 dark:bg-gray-700"
+                ></div>
+              </div>
+            </div>
+          </div>
 `;
 }
 
